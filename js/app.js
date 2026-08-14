614830118ffdcd545a6120a39923b81680f68ac4
Kristian <cquiceno609@gmail.com>
2026-08-13 19:56:05 -0500

    feat(auth): actualizar funcion de formulario login

--- js/app.js
+++ js/app.js
@@ -5,12 +5,25 @@ const authAlert = document.querySelector('.auth-alert');
 
 loginForm.addEventListener('submit', function(event) {
     event.preventDefault();
-    if (usernameInput.value === '' || passwordInput.value === '') {
-        authAlert.classList.add('show');
-    } else {
-        authAlert.classList.remove('show');
-    }
+    if (validateForm()) {
+        console.log('Formulario enviado...'); // Aquí puedes agregar la lógica para enviar el formulario al servidor
+    } 
+
 });
 
 
+function validateForm() {
+    if (usernameInput.value === '' || passwordInput.value === '') {
+        authAlert.textContent = 'Todos los campos son obligatorios.';
+        authAlert.classList.add('show');
+        return false;
+    } else if (passwordInput.value.length < 6) {
+        authAlert.textContent = 'La contraseña debe tener al menos 6 caracteres.';
+        authAlert.classList.add('show');
+        return false;
+    } else {
+        authAlert.classList.remove('show');
+        return true;
+    }   
+}
 
